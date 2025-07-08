2175d4e9fc8addcf2aa66129f445e8ccef25bee2
Harsh0726 <harshanineranjana@gmail.com>
2025-07-08 15:38:15 +0530

    News list statfull widget created

---
+++ lib/widgets/news_list.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:hot_news/controllers/news_controller.dart';
+import 'package:hot_news/models/article.dart';
+import 'package:hot_news/screens/newsdetail.dart';
+import 'package:provider/provider.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class NewsList extends StatefulWidget {
+  const NewsList({super.key});
+
+  @override
+  State<NewsList> createState() => _NewsListState();
+}
+
+class _NewsListState extends State<NewsList> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final newsController = Provider.of<NewsController>(
+        context,
+        listen: false,
+      );
+      if (newsController.status == NewsStatus.initial ||
+          newsController.articles.isEmpty) {
+        newsController.fetchTopHeadlinesUS('general');
+      }
+    });
+    super.initState();
+  }
+
+  void _selectAticle(BuildContext context, Article article) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => NewsdetailScreen(
+          articledetail: article,
+        ),
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<NewsController>(
+      builder: (context, newsController, child) {
+        return ListView.builder(
+          itemCount: newsController.articles.length,
+          itemBuilder: (context, index) {
+            final article = newsController.articles[index];
+
+            // date formatter
+            String formattedDate = '';
+            try {
+              final dateTime = DateTime.parse(article.publishedAt);
+
+              if (dateTime.hour < 12) {
+                formattedDate =
+                    '${dateTime.day}/${dateTime.month}/${dateTime.year} - ${dateTime.hour}:${dateTime.minute} AM';
+              } else {
+                formattedDate =
+                    '${dateTime.day}/${dateTime.month}/${dateTime.year} - ${dateTime.hour - 12}:${dateTime.minute} PM';
+              }
+            } catch (e) {
+              formattedDate = 'Invalid date';
+            }
+
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+              color: Colors.white,
+              shadowColor: Theme.of(context).colorScheme.primary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.primary.withAlpha(50),
+                  width: 0.5,
+                ),
+              ),
+              // card content
+              child: InkWell(
+                // onTap to navigate to article details
+                onTap: () => _selectAticle(context, article),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      // headline - title
+                      Text(
+                        article.title!,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+
+                      // author and publlished date
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              article.author ?? 'Author Unknown',
+                              style: Theme.of(context).textTheme.bodySmall,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ), // Spacing between author and date
+                          Text(
+                            formattedDate,
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+
+                      // Image
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: article.urlToImage != null
+                            ? Hero(
+                              tag: article.publishedAt, 
+                              child: FadeInImage(
+                                placeholder: MemoryImage(kTransparentImage), 
+                                image: NetworkImage(article.urlToImage!),
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                              )
+                            )
+                            : const SizedBox.shrink(),
+                      ),
+
+                      // description
+                      if (article.description != null)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            article.description!,
+                            style: Theme.of(
+                              context,
+                            ).textTheme.headlineSmall!.copyWith(fontSize: 18),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
